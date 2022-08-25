d576fe6409ab6b7490452d64c1a6f7bad4e64dbf
youixentoo <youixentoo@gmail.com>
2022-08-25 12:47:40 +0200

    json db

--- bot.py
+++ bot.py
@@ -10,8 +10,11 @@ nest_asyncio.apply()
 import os
 import logging
 import discord
+from uuid import uuid4
 from discord.ext import commands
 from dotenv import load_dotenv
+
+import json
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
@@ -30,38 +33,120 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 class ShowCodeButtonView(discord.ui.View): # Create a class called ShowCodeButtonView that subclasses discord.ui.View
-    def __init__(self, *, code, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, *, code, unique_id, **kwargs):
+        super().__init__(**kwargs, timeout=300) # I think it's in seconds
         self.code = code
+        self.unique_id = unique_id
+        
+    async def on_timeout(self):
+        for child in self.children:
+            child.disabled = True
+        await self.message.edit(content="Time limit reached, joining match disabled", view=self)
 
     @discord.ui.button(label="Show code", style=discord.ButtonStyle.primary) # Create a button with a label with color Blurple
     async def button_callback(self, button, interaction):
         # print(dir(interaction))
         print(f"Button pressed by: {interaction.user}")
-        await interaction.response.send_message(content=self.code, ephemeral=True) # Send a message when the button is clicked
+        tempSaveData(self.code, interaction.user)
+        await interaction.response.send_message(content=self.code.upper(), ephemeral=True) # Send a message when the button is clicked
+        
+        
+     
+        
+# class SetEncoder(json.JSONEncoder):
+#     def default(self, obj):
+#        if isinstance(obj, set):
+#           return list(obj)
+#        return json.JSONEncoder.default(self, obj)
+
+"""
+What to store in database:
+    - 'Primary key'
+    - Lobby code
+    - Host
+    - List of players
+    - Date created match
+    - UUID
+
+"""
 
 
 @bot.event
 async def on_ready():
-    print(f"We have logged in as {bot.user}")
+    print(f"Logged in as {bot.user}")
+    
     
 @bot.command()
 async def lobby(ctx, lobby_code, *args):
-    match_id = "insert unique id here" 
     origin = ctx.message
+    host = f"{origin.author.name}#{origin.author.discriminator}"
+    unique_id = uuid4()
     embed = discord.Embed(
         title=f"{' '.join(args)}",
-        description=f"Match id: {match_id}",
+        description=f"Match hosted by: {host}\nID: {unique_id}",
         color=discord.Colour.blurple(), # Pycord provides a class with default colors you can choose from
     )
     # print(origin)
     await origin.delete() # Deletes the command message
-    await ctx.send(view=ShowCodeButtonView(code=lobby_code), embed=embed)
+    await ctx.send(view=ShowCodeButtonView(code=lobby_code, unique_id=unique_id), embed=embed)
+    
+    
+@bot.command()
+async def getData(ctx, lobby_code):
+    match_data = tempShowData(lobby_code)
+    await ctx.send(match_data)
+    
+    
+    
+def tempLoadStorage():
+    with open("storage.json") as json_file:
+        return json.load(json_file)
+
+def tempSaveStorage(tempData):
+    with open("storage.json", "w") as json_file:
+        return json.dump(tempData, json_file)
+    
+    
+def tempSaveData(match_id, player):
+    tempData = tempLoadStorage()
+    player_string = f"{player.name}#{player.discriminator}"
+    
+    if(tempData.get(match_id)):
+        tempPlayerData = tempData[match_id]
+        if not(player_string in tempPlayerData):
+            print("player already in list")
+            tempPlayerData.append(player_string)
+        tempData[match_id] = tempPlayerData
+    else:
+        tempData[match_id] = [player_string]
+    
+    print("TempData:",tempData)
+        
+    tempSaveStorage(tempData)
+    
+    
+def tempShowData(match_id):
+    tempData = tempLoadStorage()
+    player_data = tempData.get(match_id)
+    if(player_data):
+        return "\n".join(player_data)
+    else:
+        return "None"
+    
     
  
     
  
 # Tests    
+
+@bot.command()
+async def give(ctx, *args):
+    embed = discord.Embed(
+        title=f"{''.join(args)}",
+        description="Embed desc.",
+        color=discord.Colour.blurple(), # Pycord provides a class with default colors you can choose from
+    ) 
+    await ctx.send(embed=embed) # Send the embed with some text
  
 @bot.slash_command(guild_ids=guildIDS)
 async def ephem(ctx):
@@ -73,16 +158,20 @@ async def hello(ctx):
     await ctx.respond("Hello!")
     
 @bot.slash_command(guild_ids=guildIDS)
-async def embed(ctx):
+# @option(
+#         "description",
+#         description="Embed description",
+#         required=True
+# )
+async def embed(ctx, code: str):
     embed = discord.Embed(
-        title="Embed Title",
+        title=f"{code}",
         description="Embed desc.",
         color=discord.Colour.blurple(), # Pycord provides a class with default colors you can choose from
     ) 
-    await ctx.respond("Hello! Here's a cool embed.", embed=embed) # Send the embed with some text
+    await ctx.respond(embed=embed) # Send the embed with some text
  
 # End of tests   
-
 
 bot.run(token)
 
